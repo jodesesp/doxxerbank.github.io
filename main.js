7f7c2be4c0fac67c00bd81f0608656890d5db83d
Sqwit <99897819+jodesesp@users.noreply.github.com>
2023-01-12 18:00:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function validate() {
+    let x = document.getElementById("fname").value;
+    if (x == "Alejandro6_4-cvgk435") {
+      document.getElementById("prub").style.display = ''; // show
+      alert("bienvenido " + x);
+
+    }
+    if (x == "cvgk435") {//Alejandro
+        document.getElementById("prub").style.display = ''; // show
+        alert("bienvenido " + "Alejandro6_4");
+  
+    }
+    if (x == "kjddfg342") {//Comedy
+        document.getElementById("prub").style.display = ''; // show
+        alert("bienvenido " + "comedy good");
+  
+    }
+    if (x == "ha4nbx98") {//Ruben
+        document.getElementById("prub").style.display = ''; // show
+        alert("bienvenido " + "Rubén");
+  
+    }
+    if (x == "yo2808") {//yo
+        document.getElementById("prub").style.display = ''; // show
+        alert("bienvenido " + "Hola Admin");
+  
+      }
+}
+
+document.getElementById("prub").style.display = 'none'; // hide
